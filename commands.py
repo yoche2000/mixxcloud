c12012dba1fd73d4540f09a73c034248c07aef37
jay-karan <jaykarantelukunta@gmail.com>
2024-03-27 20:10:32 +0000

    Implementation of Creation of Router - Definition & Start | Deletion of Router

--- commands.py
+++ commands.py
@@ -40,11 +40,6 @@ class VM_CRUD_Workflows:
             return False
         return True
 
-
-        
-
-
-
     @staticmethod
     def run_ansible_playbook_for_vm_deletion(vmName):
         print(f"VM Deletion for {vmName} has been triggered..")
@@ -82,6 +77,40 @@ class ROUTER_CRUD_Workflows:
         VM_CRUD_Workflows.run_ansible_playbook_for_vm_deletion(vmName)
         ROUTER_CRUD_Workflows.run_ansible_playbook_for_router_creation(vmName, vCPU, memory, diskSize, interfaces)
         print(f"Router is Stablized, subnets have been attached..")
+    
+    @staticmethod
+    def run_ansible_playbook_for_router_definition(vmName, vCPU, memory, diskSize, interfaces):
+        try:
+            print(f"Router Definition for {vmName} has been triggered..")
+            RouterConfiguration.createRouterVarsFile(vmName, vCPU, memory, diskSize, interfaces)
+            command = ["ansible-playbook", "-i", "ansible/hosts", "ansible/create-vm-router-definition.yml"]
+            Commands.run_command(command)
+            print(f"Router Definition for {vmName} has been completed..")
+        except Exception as error:
+            print(f"Router Definition for {vmName} has a failure..")
+            print(f"More Details: {error}")
+            return False
+        return True
+
+    @staticmethod
+    def run_ansible_playbook_for_router_start(vmName):
+        try:
+            print(f"Router Start for {vmName} has been triggered..")
+            command = ["ansible-playbook", "-i", "ansible/hosts", "ansible/create-vm-router-start.yml","-e",f"vm_name={vmName}"]
+            Commands.run_command(command)
+            print(f"Router Start for {vmName} has been completed..")
+        except Exception as error:
+            print(f"Router Start for {vmName} has a failure..")
+            print(f"More Details: {error}")
+            return False
+        return True
+
+    @staticmethod
+    def run_ansible_playbook_for_router_deletion(vmName):
+        print(f"Router Deletion for {vmName} has been triggered..")
+        command = ["ansible-playbook", "-i", "ansible/hosts", "ansible/destroy-vm.yml", "-e", f"vm_name={vmName}"]
+        Commands.run_command(command)
+        print(f"Router Deletion for {vmName} has been completed..")
 
 
 
@@ -112,6 +141,31 @@ ROUTER_CRUD_Workflows.run_ansible_playbook_for_router_creation(vm_name, vcpu, me
 """
 
 """
+Create Router => Definition and Start
+"""
+vm_name = "RouterVM"
+vcpu = 2
+mem = 2048
+disk_size = "12G"
+interfaces = [
+    {
+        "network_name": "host-nat-network",
+        "iface_name": "enp1s0",
+        "ipaddress": "172.16.222.2/12",
+        "dhcp": False,
+        "gateway": "172.16.0.1",
+    },
+    {
+        "network_name": "L2",
+        "iface_name": "enp2s0",
+        "ipaddress": "192.168.1.1/24",
+    }
+]
+
+ROUTER_CRUD_Workflows.run_ansible_playbook_for_router_definition(vm_name, vcpu, mem, disk_size, interfaces)
+ROUTER_CRUD_Workflows.run_ansible_playbook_for_router_start(vm_name)
+
+"""
 2. VM Creation
 
 vm_name = "ProjectGuestVM"
@@ -132,7 +186,7 @@ VM_CRUD_Workflows.run_ansible_playbook_for_vm_creation(vm_name, vcpu, mem, disk_
 
 """
 2. VM Creation - Define
-"""
+
 vm_name = "ProjectGuestVM-1"
 vcpu = 2
 mem = 2048
@@ -149,12 +203,12 @@ interfaces = [
 
 VM_CRUD_Workflows.run_ansible_playbook_for_vm_definition(vm_name, vcpu, mem, disk_size, interfaces)
 VM_CRUD_Workflows.run_ansible_playbook_for_vm_start(vm_name)
+"""
 
 """
 3. Deletion of VM
 
-
-VM_CRUD_Workflows.run_ansible_playbook_for_vm_deletion("ProjectGuestVM")
+VM_CRUD_Workflows.run_ansible_playbook_for_vm_deletion("RouterVM")
 """
 
 """
