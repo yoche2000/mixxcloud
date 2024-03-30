ed5bf669ebfbc84fe378c581898895ab7e7336f5
jay-karan <jaykarantelukunta@gmail.com>
2024-03-30 16:15:08 +0000

    Tenant Login Prompt

--- cli/console.py
+++ cli/console.py
@@ -1,14 +1,31 @@
 import click
 import shutil
 import pyfiglet
-from controllers import TenantController
+from controllers.tenant_controller import TenantController
 from models.db_model import DB
 import traceback
 # sudo apt-get install -y python3-pyfiglet
 # Usage from main.py: sudo python3 main.py [command]
 # Usage from console.py: sudo python3 console.py [command]
 
+def dbclient():
+    db = DB()
+    try:
+        client = db.get_client()
+    except Exception:
+        traceback.print_exc()
+        exit(1)
+    db = client['ln']
+    return db
+db = dbclient()
 
+tenantName = None
+
+def login():
+    global tenantName
+    click.echo()
+    tenantName = click.prompt(click.style("Enter your User ID: ", fg='yellow'), type=str)
+    click.echo()
 
 def display_welcome(title = pyfiglet.figlet_format("- MIXXCLOUD -", font="slant"), message="Welcome to the Cloud Implementation by Sumalatha, Thomas, Karan & Deepak!! \U0001F44B \U0001F44B"):
     click.secho(title, fg='blue', bold=True)
@@ -54,6 +71,7 @@ def tenant():
 @cli.command()
 def vpc():
     display_welcome(title = pyfiglet.figlet_format("- VPC Console -", font="digital"), message="Choose an action to continue:")
+    print(f"Tenant Name:", tenantName)
     click.secho("1: Create VPC", fg='cyan')
     click.secho("2: Attach Subnet to VPC", fg='cyan')
     click.secho("3: Detach Subnet to VPC", fg='cyan')
@@ -61,8 +79,6 @@ def vpc():
     click.secho("5: List VPCs", fg='cyan')
     click.secho("6: Create Subnet", fg='cyan')
     click.secho("7: Delete Tenant", fg='cyan')
-
-    
 
     # Create VPC
     click.echo()
@@ -78,24 +94,13 @@ def vpc():
     else:
         pass
 
-def dbclient():
-    db = DB()
-    try:
-        client = db.get_client()
-    except Exception:
-        traceback.print_exc()
-        exit(1)
-    db = client['ln']
-    return db
-
-db = dbclient()
-
 @cli.command()
 def loadbalancer():
     display_welcome(title = pyfiglet.figlet_format("- Load Balancer Console -", font="digital"), message="Choose an action to continue:")
 
 def main():
     display_welcome()
+    login()
     cli()
 
 if __name__ == "__main__":
