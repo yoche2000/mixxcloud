c4f787b2060df4c4943cdd553dab2c4a634e8b64
yoche2000 <yoche2000@gmail.com>
2024-04-21 21:02:31 +0000

    added docker files in repo

---
+++ docker/php-info/ipinfo.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Server IP Address</title>
+</head>
+<body>
+    <h1>Server IP Address</h1>
+    <p><?php echo $_SERVER['SERVER_ADDR']; ?></p>
+</body>
+</html>
